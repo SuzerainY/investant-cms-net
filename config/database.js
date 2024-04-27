f5ad99a106359ce6b5083e7a2a0d9969a6379a64
Ryan White <white.ryanw@gmail.com>
2024-04-27 15:14:19 -0700

    Added about us page profile images

--- config/database.js
+++ config/database.js
@@ -55,6 +55,7 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
@@ -63,10 +64,9 @@ module.exports = ({ env }) => {
           cipher: env('DATABASE_SSL_CIPHER', undefined),
           rejectUnauthorized: env.bool(
             'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
+            false
           ),
         },
-        schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,17 @@
- // The purpose of this file is to define the postgres database which is used in production. When the app is in development, however, the /config/database.js file is used
- module.exports = ({ env }) => ({
+// The purpose of this file is to define the postgres database which is used in production. When the app is in development, however, the /config/database.js file is used
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT, // May need to cast to integer
-        database: process.env.DATABASE_NAME,
-        user: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        ssl: {
-          ca: process.env.DATABASE_CA,
-        },
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      ssl: {
+        ca: process.env.DATABASE_CA,
       },
-      debug: false,
     },
-  });
+    debug: false,
+  },
+});
