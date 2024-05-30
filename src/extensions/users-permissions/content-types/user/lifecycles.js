71bb78ab44f59a7181677dfc3d6e827ee7f3e42a
Ryan White <white.ryanw@gmail.com>
2024-05-29 23:06:44 -0700

    Do not console log

--- src/extensions/users-permissions/content-types/user/lifecycles.js
+++ src/extensions/users-permissions/content-types/user/lifecycles.js
@@ -1,10 +1,7 @@
 module.exports = {
     async afterCreate({ result }) {
-        console.log("New user created");
-
         // If the brand new user was just created as confirmed, then update them back to not confirmed
         if (result.confirmed === true) {
-            console.log("User was confirmed");
             await strapi.plugin('users-permissions').services.user.edit(result.id, { confirmed: false });
         }
     },
