e357f5ff5b8e40f15b96714e397be142fcef6d6f
Ryan White <white.ryanw@gmail.com>
2024-06-29 16:11:04 -0700

    Take UTC date from contact us submission and convert to Eastern.

--- src/api/contact-us-submission/content-types/contact-us-submission/lifecycles.js
+++ src/api/contact-us-submission/content-types/contact-us-submission/lifecycles.js
@@ -13,7 +13,27 @@ const formatDate = (date) => {
     hour = hour % 12;
     hour = hour ? hour : 12; // the hour '0' should be '12'
 
-    return `${hour}:${minute} ${ampm} on ${month} ${day}, ${year}`;
+    return `${hour}:${minute} ${ampm} on ${month} ${day}, ${year} Eastern`;
+};
+
+// Convert UTC date to Eastern Time. Eastern Standard Time (EST) is UTC-5. Eastern Daylight Time (EDT) is UTC-4
+const convertUTCtoEastern = (date) => {
+    // Get the offset in minutes
+    const offset = date.getTimezoneOffset(); // in minutes
+    const utcOffset = -offset / 60; // convert to hours
+
+    let estOffset = -5;
+    // Check if Daylight Saving Time is in effect
+    const isDST = (date) => {
+        const jan = new Date(date.getFullYear(), 0, 1).getTimezoneOffset();
+        const jul = new Date(date.getFullYear(), 6, 1).getTimezoneOffset();
+        return Math.max(jan, jul) !== date.getTimezoneOffset();
+    };
+    if (isDST(date)) {estOffset = -4;}
+
+    // Calculate the time difference in milliseconds
+    const timeDifference = (estOffset - utcOffset) * 60 * 60 * 1000;
+    return new Date(date.getTime() + timeDifference);
 };
 
 module.exports = {
@@ -29,13 +49,14 @@ module.exports = {
 
             // Add HTML for new line characters to the message
             let parsedMessage = result.Message.replace(/\r\n/g, '<br>').replace(/\n/g, '<br>');
+            let openedAtEastern = convertUTCtoEastern(new Date(result.OpenedAt));
             
             const emailHTML = userContactUsFormSubmissionNotification({
                 submissionID: result.id,
                 submissionAdminLink: `${strapiURL}/admin/content-manager/collectionType/api::contact-us-submission.contact-us-submission/${result.id}`,
                 contactName: result.ContactName,
                 contactEmail: result.ContactEmail,
-                openedAt: formatDate(new Date(result.OpenedAt)),
+                openedAt: formatDate(openedAtEastern),
                 subject: result.Subject,
                 message: parsedMessage
             });
