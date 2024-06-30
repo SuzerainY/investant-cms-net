5a8948d29aa57ecaf77752229f9d0821483af0f5
Ryan White <white.ryanw@gmail.com>
2024-06-29 18:17:19 -0700

    do not change after eastern offset applied.

--- src/api/contact-us-submission/content-types/contact-us-submission/lifecycles.js
+++ src/api/contact-us-submission/content-types/contact-us-submission/lifecycles.js
@@ -34,7 +34,7 @@ module.exports = {
             // Set time to Eastern
             let openedAtEastern = convertUTCtoEastern(result.OpenedAt);
             await strapi.entityService.update('api::contact-us-submission.contact-us-submission', result.id, {
-                data: {OpenedAt: openedAtEastern.toDateString()}
+                data: {OpenedAt: openedAtEastern}
             });
 
             // Check if the ContactEmail matches an existing user and add relation if so
