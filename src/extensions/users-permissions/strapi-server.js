5fb21b61dc3a3ff5cced05f0345874a6b31b4893
Ryan White <white.ryanw@gmail.com>
2024-08-10 22:34:28 -0700

    created deleteMe handler for users on api: /api/user/me DELETE method

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -46,11 +46,49 @@ module.exports = (plugin) => {
             }
         }
     };
-    // Push our controller as the handler for new content-api route: /user/me
+    // Push our controller as the PUT handler for new content-api route: /user/me
     plugin.routes['content-api'].routes.push({
         method: "PUT",
         path: "/user/me",
         handler: "user.updateMe",
+        config: {
+            prefix: "",
+            policies: []
+        }
+    });
+
+    // Controller extension for the DELETE method on /user/me route of users-permissions plugin content-api
+    plugin.controllers.user.deleteMe = async (ctx) => {
+        const sanitizeOutput = async (user, ctx) => {
+            const { sanitize } = require('@strapi/utils');
+            const schema = strapi.getModel('plugin::users-permissions.user');
+            const { auth } = ctx.state;
+            
+            return sanitize.contentAPI.output(user, schema, { auth });
+        };
+        
+        if (!ctx.state.user || !ctx.state.user.id) {return ctx.unauthorized();}
+
+        try {
+            // Get the authenticated user ID
+            const userId = ctx.state.user.id;
+
+            // Proceed with account deletion
+            const data = await strapi.plugin('users-permissions').service('user').remove({ id: userId });
+            const sanitizedUser = await sanitizeOutput(data, ctx);
+
+            ctx.send(sanitizedUser);
+        } catch (error) {
+            const errorMessage = error.message;
+            ctx.response.status = 500;
+            ctx.response.body = { error: 'Unable to delete user', details: errorMessage };
+        }
+    };
+    // Push our controller as the DELETE handler for new content-api route: /user/me
+    plugin.routes['content-api'].routes.push({
+        method: "DELETE",
+        path: "/user/me",
+        handler: "user.deleteMe",
         config: {
             prefix: "",
             policies: []
